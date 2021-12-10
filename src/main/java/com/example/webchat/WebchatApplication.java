70ac80e66a4ecd545694f883ef713dbb50782e6a
RZX <850631577@qq.com>
2021-12-10 19:23:51 +0800

    14zhou

---
+++ src/main/java/com/example/webchat/WebchatApplication.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) throws UnknownHostException {
+        ConfigurableApplicationContext application = SpringApplication.run(DemoApplication.class, args);
+        Environment env = application.getEnvironment();
+        String host = InetAddress.getLocalHost().getHostAddress();
+        String port = env.getProperty("server.port");
+        System.out.println("[----------------------------------------------------------]");
+        System.out.println("聊天室启动成功！点击进入:\t http://" + host + ":" + port);
+        System.out.println("[----------------------------------------------------------");
+        WebSocketServer.inst().run(53134);
+    }
+
+}
